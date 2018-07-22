e16e359e32f5c212cf008b8a45bada5e544e4148
toddlerya <toddlerya@gmail.com>
2018-07-22 16:21:29 +0800

    台风来了

--- static/script/example.js
+++ static/script/example.js
@@ -1,7 +1,13 @@
 window.onload = function() {
-    var testdiv = document.getElementById("testdiv");
     var para = this.document.createElement("p");
+    var txt1 = document.createTextNode("This is ");
+    para.appendChild(txt1);
+    var emphasis = document.createElement("em");
+    var txt2 = document.createTextNode("my");
+    emphasis.appendChild(txt2);
+    para.appendChild(emphasis);
+    var txt3 = this.document.createTextNode(" content.");
+    para.appendChild(txt3);
+    var testdiv = this.document.getElementById("testdiv");
     testdiv.appendChild(para);
-    var txt = document.createTextNode("Hello world");
-    para.appendChild(txt);
 }
--- static/script/showPic.js
+++ static/script/showPic.js
@@ -1,3 +1,56 @@
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    // gallery.parentNode.insertBefore(placeholder, gallery);
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
+
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName || !document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this) ? false : true;
+        }
+    }
+}
+
 function showPic(whichpic) {
     if (!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
@@ -18,51 +71,6 @@ function showPic(whichpic) {
     return true;
 }
 
-function countBodyChildren() {
-    var body_element = document.getElementsByTagName("body")[0];
-    alert(body_element.nodeType);
-}
 
-function popUp(winURL) {
-    window.open(winURL, "popup", "width=320, height=480");
-}
-
-function prepareLinks() {
-    if (!document.getElementsByName) return false;
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].getAttribute("class") == "popup") {
-            links[i].onclick = function () {
-                popUp(this.getAttribute("href"));
-                return false;
-            }
-        }
-    }
-}
-
-function prepareGallery() {
-    if (!document.getElementsByTagName || !document.getElementById) return false;
-    if (!document.getElementById("imagegallery")) return false;
-    var gallery = document.getElementById("imagegallery");
-    var links = gallery.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        links[i].onclick = function () {
-            return showPic(this) ? false : true;
-        }
-    }
-}
-
-function addLoadEvent(func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function () {
-            oldonload();
-            func();
-        }
-    }
-}
-
-addLoadEvent(prepareLinks);
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
